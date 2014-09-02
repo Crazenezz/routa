1c929069d2842ac26828276c96346675aee95235
Crazenezz <crazenezz@gmail.com>
2014-09-02 21:39:48 +0700

    Add Not Found Page controller

---
+++ app/controller/Not-Found.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controller;
+use app\lib\Controller;
+use app\lib\View;
+
+class Not_Found extends Controller {
+
+    public function run() {
+        View::show('404');
+    }
+}
+
+?>
