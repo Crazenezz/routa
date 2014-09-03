e27054eba8041256f8a3c83ac8f964dfc0aeb5cb
Edward Tumuwo <edward@xituz.com>
2014-09-03 18:52:15 +0700

    Add config file

--- app/test/RouteTest.php
+++ app/test/RouteTest.php
@@ -6,7 +6,7 @@ use app\lib\Route;
 class RouteTest extends \PHPUnit_Framework_TestCase {
     
     public function __construct() {
-        define('PATH_BASE', realpath(dirname(__FILE__)).'/../../');
+        include '../../config.php';
     }
     
     public function testBaseHref() {
--- index.php
+++ index.php
@@ -1,25 +1,14 @@
 <?php
 
     /**
-     *  Debug configuration
+     *  Configuration
      */
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-    ini_set('html_errors', 1);
-	ini_set('log_errors', 1);
-    
-    /**
-     *  Define global path
-     */
-    define('PATH_BASE', realpath(dirname(__FILE__)).'/');
-    define('PATH_APP', PATH_BASE.'/app/');
-    define('PATH_VIEW', PATH_APP.'/view/');
-    define('PATH_CONTROLLER', PATH_APP.'/controller/');
+    include 'config.php';
     
     /**
      *  Function autoload
      */
-    include PATH_APP.'autoload.php';
+    include 'app/autoload.php';
     
     /**
      *  Routing setup
